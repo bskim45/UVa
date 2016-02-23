f260cba4d935d427475696f272e3861d1f9f267b
Bumsoo Kim <bskim45@gmail.com>
2016-02-23 19:00:09 +0900

    p11002 - Towards Zero: time tweak

--- p11002 - Towards Zero.cpp
+++ p11002 - Towards Zero.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
-#include <algorithm>
-#include <unordered_map>
+#include <cstring>
 
+constexpr int MAX_SUM = 100;
 using namespace std;
 
 int board[70][70] = { 0 };
-unordered_map<int, bool> sums[70][70];
+bool sum_pos[70][70][MAX_SUM];
+bool sum_neg[70][70][MAX_SUM];
 
-void init() {
-    for (int i = 0; i < 70; i++) {
-        for (int j = 0; j < 70; j++) {
-            board[i][j] = -1;
-            sums[i][j].clear();
-        }
-    }
+void insert(int row, int col, int value) {
+    if (value >= 0)
+        sum_pos[row][col][value] = true;
+    else
+        sum_neg[row][col][-value] = true;
 }
-
 
 int main() {
     ios::sync_with_stdio(false);
@@ -24,7 +22,9 @@ int main() {
     cin >> rows;
 
     while (rows != 0) {
-        init();
+        memset(board, -1, sizeof board);
+        memset(sum_pos, false, sizeof sum_pos);
+        memset(sum_neg, false, sizeof sum_neg);
 
         int input = 0;
         for (int i = 1; i <= rows * 2 - 1; i++) {
@@ -35,54 +35,99 @@ int main() {
             }
         }
 
-        sums[2 * rows - 1][1][board[2 * rows - 1][1]] = true;
-        sums[2 * rows - 1][1][-board[2 * rows - 1][1]] = true;
+        insert(2 * rows - 1, 1, board[2 * rows - 1][1]);
+        insert(2 * rows - 1, 1, -board[2 * rows - 1][1]);
 
         for (int i = rows * 2 - 1; i >= 2; i--) {
             if (i > rows) {
                 for (int j = 1; j <= rows * 2 - i; j++) {
-                    for (auto iter = sums[i][j].begin(); iter != sums[i][j].end(); ++iter) {
-                        if (board[i - 1][j] != -1) {
+                    for (int k = 0; k < MAX_SUM; k++) {
+                        if (!sum_pos[i][j][k] && !sum_neg[i][j][k])
+                            continue;
+                        
+                        if (sum_pos[i][j][k]) {
+                            if (board[i - 1][j] != -1) {
 
-                            // left
-                            sums[i - 1][j][iter->first + board[i - 1][j]] = true;
-                            sums[i - 1][j][iter->first - board[i - 1][j]] = true;
+                                // left
+                                insert(i - 1, j, k + board[i - 1][j]);
+                                insert(i - 1, j, k - board[i - 1][j]);
+                            }
+
+                            if (board[i - 1][j + 1] != -1) {
+
+                                // right
+                                insert(i - 1, j + 1, k + board[i - 1][j + 1]);
+                                insert(i - 1, j + 1, k - board[i - 1][j + 1]);
+                            }
                         }
+                        
+                        if (sum_neg[i][j][k]) {
+                            if (board[i - 1][j] != -1) {
 
-                        if (board[i - 1][j + 1] != -1) {
+                                // left
+                                insert(i - 1, j, -k + board[i - 1][j]);
+                                insert(i - 1, j, -k - board[i - 1][j]);
+                            }
 
-                            // right
-                            sums[i - 1][j + 1][iter->first + board[i - 1][j + 1]] = true;
-                            sums[i - 1][j + 1][iter->first - board[i - 1][j + 1]] = true;
+                            if (board[i - 1][j + 1] != -1) {
+
+                                // right
+                                insert(i - 1, j + 1, -k + board[i - 1][j + 1]);
+                                insert(i - 1, j + 1, -k - board[i - 1][j + 1]);
+                            }
                         }
                     }
                 }
             }
             else {
                 for (int j = 1; j <= i; j++) {
-                    for (auto iter = sums[i][j].begin(); iter != sums[i][j].end(); ++iter) {
-                        if (board[i - 1][j - 1] != -1) {
-                            // left
-                            sums[i - 1][j - 1][iter->first + board[i - 1][j - 1]] = true;
-                            sums[i - 1][j - 1][iter->first - board[i - 1][j - 1]] = true;
+                    for (int k = 0; k < MAX_SUM; k++) {
+                        if (!sum_pos[i][j][k] && !sum_neg[i][j][k])
+                            continue;
+
+                        if (sum_pos[i][j][k]) {
+
+                            if (board[i - 1][j - 1] != -1) {
+                                // left
+                                insert(i - 1, j - 1, k + board[i - 1][j - 1]);
+                                insert(i - 1, j - 1, k - board[i - 1][j - 1]);
+                            }
+
+                            if (board[i - 1][j] != -1) {
+                                // right
+                                insert(i - 1, j, k + board[i - 1][j]);
+                                insert(i - 1, j, k - board[i - 1][j]);
+                            }
                         }
 
-                        if (board[i - 1][j] != -1) {
-                            // right
-                            sums[i - 1][j][iter->first + board[i - 1][j]] = true;
-                            sums[i - 1][j][iter->first - board[i - 1][j]] = true;
+                        if(sum_neg[i][j][k]) {
+                            if (board[i - 1][j - 1] != -1) {
+                                // left
+                                insert(i - 1, j - 1, -k + board[i - 1][j - 1]);
+                                insert(i - 1, j - 1, -k - board[i - 1][j - 1]);
+                            }
+
+                            if (board[i - 1][j] != -1) {
+                                // right
+                                insert(i - 1, j, -k + board[i - 1][j]);
+                                insert(i - 1, j, -k - board[i - 1][j]);
+                            }
                         }
                     }
                 }
             }
         }
 
-        auto min_elem = min_element(sums[1][1].begin(), sums[1][1].end(), [](const pair<int, bool> &a, const pair<int, bool> &b)
-        {
-            return abs(a.first) < abs(b.first);
-        });
+        int min = MAX_SUM;
 
-        cout << abs(min_elem->first) << endl;
+        for (int i = 0; i < MAX_SUM; i++) {
+            if (sum_pos[1][1][i] || sum_neg[1][1][i]) {
+                min = i;
+                break;
+            }
+        }
+
+        cout << min << endl;
         cin >> rows;
     }
 
